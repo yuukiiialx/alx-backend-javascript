6abdf09ceea9361f34dc7de2aeca468fdf9988c9
Mohamed <alxmohamedemadeldin@gmail.com>
2024-06-25 15:59:31 +0000

    Added Repo

--- 1-promise.js
+++
@@ -1,6 +0,0 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) return resolve({ status: 200, body: 'Success' });
-    return reject(Error('The fake API is not working currently'));
-  });
-}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x01-ES6_promise
+solving js backend
