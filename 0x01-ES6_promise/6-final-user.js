4fbffeb2e1423e0cd196af6f0012e56a5c0ccb5e
Mohamed <alxmohamedemadeldin@gmail.com>
2024-06-25 16:00:13 +0000

    Solved tasks

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    for (const value of values) {
+      if (value.status === 'rejected') {
+        value.value = `${String(value.reason)}`;
+        delete value.reason;
+      }
+    }
+    return values;
+  });
+}
